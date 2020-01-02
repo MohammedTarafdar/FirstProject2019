6674f3d21ac98c6b50ed0cbc4fbe49919a62b3a1
Mohammed Tarafdar <MohammedTarafdar>
2020-01-02 11:38:26 -0500

    class ArithmeticOperators updated

---
+++ src/main/java/basic/operators/ArithmeticOperators.java
@@ -0,0 +1,27 @@
+package basic.operators;
+
+public class ArithmeticOperators {
+
+    /*
+    The basic arithmetic operators are:
+    + is for addition
+    - is for subtraction
+    * is for multiplication
+    / is for division
+    % is for modulo
+     */
+
+    public static void main(String[] args) {
+        int num1 = 10, num2 =3;
+
+        System.out.println("Sum of num1 and num2 is: "+(num1+num2));
+        System.out.println("Subtraction of num1 and num2 is: "+(num1-num2));
+        System.out.println("Multiplication of num1 and num2 is: "+(num1*num2));
+        System.out.println("Division of num1 and num2 is: "+(num1/num2));
+        System.out.println("Modulus of num1 and num2 is: "+(num1%num2));
+
+    }
+
+
+
+}
